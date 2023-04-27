62c1f7021076612ffdd3fe23b083452dd4489c0b
cowsay-dev <nileshsuryavanshi@protonmail.com>
2023-04-27 17:42:24 +0530

    fixed UI glitches in profile page

--- src/skeletons/UserProflieSkeleton.tsx
+++ src/skeletons/UserProflieSkeleton.tsx
@@ -6,13 +6,13 @@ const UserProflieSkeleton = () => {
     <>
       <div className="user-profile-skeleton">
         <div className="user-profile-skeleton-detail" id="ups-1">
-          <Skeleton height={52} width={100} />
+          <Skeleton height={52} width={64} />
         </div>
         <div className="user-profile-skeleton-avatar" id="ups-2">
           <Skeleton circle width={120} height={120} />
         </div>
         <div className="user-profile-skeleton-detail" id="ups-3">
-          <Skeleton height={52} width={100} />
+          <Skeleton height={52} width={64} />
         </div>
       </div>
       <div className="user-profile-skeleton-name">
